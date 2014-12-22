d911e9e3b51a5fa0b73719a845a8eb8059144145
konfiot <arthurtoussaint@free.fr>
2014-12-22 22:49:14 +0100

    New Feature : Fonction d'envoi des données série

---
+++ fsk.py
@@ -0,0 +1,53 @@
+# coding=utf-8
+
+# Bell 202 Audio Frequency Shift Keying
+# http://n1vg.net/packet/
+
+import logging
+logger = logging.getLogger(__name__)
+
+import math
+import itertools
+import sys
+import bitarray
+import audiogen
+
+MARK_HZ = 900.0
+SPACE_HZ = 1500.0
+BAUD_RATE = 600.0
+
+TWO_PI = 2.0 * math.pi
+
+def encode(data):
+	for sample in itertools.chain.from_iterable([modulatebyte(c) for c in data]):
+		yield sample
+
+def modulatebyte(byte):
+	seconds_per_sample = 1.0 / audiogen.sampler.FRAME_RATE
+	phase, seconds, bits = 0, 0, 0
+
+	# construct generators
+	clock = (x / BAUD_RATE for x in itertools.count(1))
+	tones = (MARK_HZ if i == 0 or ((ord(byte) >> (i-1)) & 1) else SPACE_HZ for i in range(10))
+
+	for boundary, frequency in itertools.izip(clock, tones):
+		# frequency of current symbol is determined by how much 
+		# we advance the signal's phase in each audio frame
+		phase_change_per_sample = TWO_PI / (audiogen.sampler.FRAME_RATE / frequency)
+
+		# produce samples for the current symbol 
+		# until we reach the next clock boundary
+		while seconds < boundary:
+			yield math.sin(phase)
+
+			seconds += seconds_per_sample
+			phase += phase_change_per_sample
+
+			if phase > TWO_PI:
+				phase -= TWO_PI
+
+		bits += 1
+		#print("bits = %d, time = %.7f ms, expected time = %.7f ms, error = %.7f ms, baud rate = %.6f Hz" \
+		#	% (bits, 1000 * seconds, 1000 * bits / BAUD_RATE, 1000 * (seconds - bits / BAUD_RATE), bits / seconds))
+
+audiogen.sampler.write_wav(sys.stdout, encode(b"Dandandan"))
