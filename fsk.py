f7f591a08132a161a90089c17d28f25f31fed3c8
konfiot <arthurtoussaint@free.fr>
2014-12-22 23:21:52 +0100

    Plus besoin de bitarray

--- fsk.py
+++ fsk.py
@@ -9,7 +9,6 @@ logger = logging.getLogger(__name__)
 import math
 import itertools
 import sys
-import bitarray
 import audiogen
 
 MARK_HZ = 900.0
@@ -31,11 +30,11 @@ def modulatebyte(byte):
 	tones = (MARK_HZ if i == 0 or ((ord(byte) >> (i-1)) & 1) else SPACE_HZ for i in range(10))
 
 	for boundary, frequency in itertools.izip(clock, tones):
-		# frequency of current symbol is determined by how much 
+		# frequency of current symbol is determined by how much
 		# we advance the signal's phase in each audio frame
 		phase_change_per_sample = TWO_PI / (audiogen.sampler.FRAME_RATE / frequency)
 
-		# produce samples for the current symbol 
+		# produce samples for the current symbol
 		# until we reach the next clock boundary
 		while seconds < boundary:
 			yield math.sin(phase)
@@ -50,4 +49,4 @@ def modulatebyte(byte):
 		#print("bits = %d, time = %.7f ms, expected time = %.7f ms, error = %.7f ms, baud rate = %.6f Hz" \
 		#	% (bits, 1000 * seconds, 1000 * bits / BAUD_RATE, 1000 * (seconds - bits / BAUD_RATE), bits / seconds))
 
-audiogen.sampler.write_wav(sys.stdout, encode(b"Dandandan"))
+kudiogen.sampler.write_wav(sys.stdout, encode(b"Dandandan"))
