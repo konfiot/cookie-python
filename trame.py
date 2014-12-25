c2aaac1860a40b6c7c0a2b9f5a38ff2949c47612
konfiot <arthurtoussaint@free.fr>
2014-12-25 13:28:08 +0100

    On supporte d'autre types de variables dans la trame (bytes, str, int)

--- trame.py
+++ trame.py
@@ -7,15 +7,22 @@ def trame(data):
     conf = json.load(open(CONF_FILE, "r"))
 
     out = bytearray([0xFF])
+
     for i, val in enumerate(data): # Concaténation des valeurs des capteurs
-        out += val.to_bytes(conf["sensors"][i]["length"], byteorder='big', signed=conf["sensors"][i]["signed"])
+        if type(val) is int:
+            out += val.to_bytes(conf["sensors"][i]["length"], byteorder='big', signed=conf["sensors"][i]["signed"])
+        elif type(val) is bytes or type(val) is bytearray :
+            out += val
+        elif type(val) is str:
+            out += val.encode("ASCII")
+
     cs = 0
 
     for b in out: # Ajout du checksum XOR
         cs ^= b
     out += cs.to_bytes(1, byteorder="big", signed = False)
 
-    rs = RSCodec(20)
+    rs = RSCodec(conf["trame"]["ecc"]["length"])
     out = rs.encode(out)
 
     return out
