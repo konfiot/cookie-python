24367b23d8f44f0caf94c3e5dd49e28db87854fe
konfiot <arthurtoussaint@free.fr>
2015-02-02 21:39:05 +0100

    Codage de la répétition de trames

--- random_trame.py
+++ random_trame.py
@@ -5,6 +5,7 @@ import json
 import struct
 import os
 import sys
+import time
 
 parser = argparse.ArgumentParser(description='Generates a random trame')
 parser.add_argument('--length', type=int, default=5, help='Number of sensors (defaults to 5)')
@@ -13,6 +14,8 @@ parser.add_argument('--ecclength', default=10, type=int, help='length of the ree
 parser.add_argument('--startbyte', default=0xFF, type=int, help='Start byte (defaults to 0xFF)')
 parser.add_argument('--file', type=str, help='Input config file')
 parser.add_argument('--out', default=sys.stdout.buffer, type=argparse.FileType('wb'), help='Output file (defaults to stdout)')
+parser.add_argument('-N', type=int, default=-1, help='Number of frames to be generated (defaults to infinite)')
+parser.add_argument('-t', type=int, default=1, help='Time between two frames (defaults to 1s)')
 args = parser.parse_args()
 
 if args.file :
@@ -20,4 +23,10 @@ if args.file :
 else :
     conf = {"sensors": [args.valtype]*args.length, "trame":{"startbyte": args.startbyte, "ecc":{"length":args.ecclength}}}
 
-args.out.write(trame.trame(struct.unpack(''.join(conf["sensors"]), os.urandom(struct.calcsize(''.join(conf["sensors"])))), conf))
+N = args.N
+while N != 0:
+	args.out.write(trame.trame(struct.unpack(''.join(conf["sensors"]), os.urandom(struct.calcsize(''.join(conf["sensors"])))), conf))
+	N -= 1
+	args.out.flush()
+	time.sleep(args.t)
+
